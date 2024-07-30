17e2a98a810bd1de1c2227289886f62f9a42e3f0
Gaurav Singh <2022pietcagaurav022@poornima.org>
2024-07-30 18:33:41 +0530

    tan tana tan

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,31 @@ import 'package:marriage_bureau_flutter/pages/home.dart';
 import 'package:marriage_bureau_flutter/pages/login.dart';
 import 'package:marriage_bureau_flutter/pages/signup.dart';
 import 'package:marriage_bureau_flutter/pages/loading.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MaterialApp(
-  initialRoute: '/',
-  routes: {
-    '/': (context) => const Loading(),
-    '/home': (context) => const Home(),
-    '/login': (context) => const Login(),
-    '/signup': (context) => const Signup(),
-  },
-));
+
+// void main() => runApp(MaterialApp(
+  
+//   initialRoute: '/',
+//   routes: {
+//     '/': (context) => const Loading(),
+//     '/home': (context) => const Home(),
+//     '/login': (context) => const Login(),
+//     '/signup': (context) => const Signup(),
+//   },
+// ));
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const Loading(),
+      '/home': (context) => const Home(),
+      '/login': (context) => const Login(),
+      '/signup': (context) => const Signup(),
+    },
+  ));
+}
